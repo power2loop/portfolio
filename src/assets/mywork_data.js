fe6e9e6412c32d8eb54816fdf7372919419f65ba
power2loop <kumar.vkvikash3@gmail.com>
2025-09-03 15:36:50 +0530

    new project

--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -4,47 +4,55 @@ import project3_img from '../assets/P3.png'
 import project4_img from '../assets/P4.png'
 import project5_img from '../assets/P5.png'
 import project6_img from '../assets/P6.png'
+import project7_img from '../assets/P7.png'
 
 const mywork_data = [
     {
         w_no: 1,
         w_name: "Web design",
         w_img: project1_img,
-        github: "https://github.com/power2loop/password-saver",
-        live: "https://password-saver-two.vercel.app/"
+        github: "https://github.com/power2loop/Whiteboard",
+        live: "https://whiteboard-1kfc.onrender.com/"
     },
     {
         w_no: 2,
         w_name: "Web design",
         w_img: project2_img,
-        github: "https://github.com/power2loop/dairy-go",
-        live: "https://dairy-go.vercel.app/"
+        github: "https://github.com/power2loop/password-saver",
+        live: "https://password-saver-two.vercel.app/"
     },
     {
         w_no: 3,
         w_name: "Web design",
         w_img: project3_img,
+        github: "https://github.com/power2loop/dairy-go",
+        live: "https://dairy-go.vercel.app/"
+    },
+    {
+        w_no: 4,
+        w_name: "Web design",
+        w_img: project4_img,
         github: "https://github.com/power2loop/movie_recommender_system",
         live: "https://movierecommendersystem1212.streamlit.app/"
     },
     {
-        w_no: 4,
+        w_no: 5,
         w_name: "Web design",
         w_img: project5_img,
         github: "https://github.com/power2loop/Weather-App",
         live: "https://weather-app-psi-eight-12.vercel.app/"
     },
     {
-        w_no: 5,
+        w_no: 6,
         w_name: "Web design",
-        w_img: project4_img,
+        w_img: project6_img,
         github: "https://github.com/power2loop/To-Do-List",
         live: "https://to-do-list-cyan-kappa-83.vercel.app/"
     },
     {
-        w_no: 6,
+        w_no: 7,
         w_name: "Web design",
-        w_img: project6_img,
+        w_img: project7_img,
         github: "https://github.com/power2loop/Text-to-speech",
         live: "https://text-to-speech-ten-wine.vercel.app/"
     },

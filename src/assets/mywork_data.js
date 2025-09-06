4cb0039a3b59b81b380d5b6adbbd936ceb39ed4d
power2loop <kumar.vkvikash3@gmail.com>
2025-09-06 19:13:27 +0530

    new project

--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -3,8 +3,10 @@ import project2_img from '../assets/P2.png'
 import project3_img from '../assets/P3.png'
 import project4_img from '../assets/P4.png'
 import project5_img from '../assets/P5.png'
+
 import project6_img from '../assets/P6.png'
 import project7_img from '../assets/P7.png'
+import project8_img from '../assets/P8.png'
 
 const mywork_data = [
     {
@@ -38,14 +40,14 @@ const mywork_data = [
     {
         w_no: 5,
         w_name: "Web design",
-        w_img: project6_img,
-        github: "https://github.com/power2loop/Weather-App",
-        live: "https://weather-app-psi-eight-12.vercel.app/"
+        w_img: project5_img,
+        github: "https://github.com/power2loop/SchoolSearch",
+        live: "https://schoolsearch-kqy9.onrender.com/"
     },
     {
         w_no: 6,
         w_name: "Web design",
-        w_img: project5_img,
+        w_img: project6_img,
         github: "https://github.com/power2loop/To-Do-List",
         live: "https://to-do-list-cyan-kappa-83.vercel.app/"
     },
@@ -53,6 +55,12 @@ const mywork_data = [
         w_no: 7,
         w_name: "Web design",
         w_img: project7_img,
+        github: "https://github.com/power2loop/Weather-App",
+        live: "https://weather-app-psi-eight-12.vercel.app/"
+    }, {
+        w_no: 8,
+        w_name: "Web design",
+        w_img: project8_img,
         github: "https://github.com/power2loop/Text-to-speech",
         live: "https://text-to-speech-ten-wine.vercel.app/"
     },
